cb073398411b7c3ad6c43f5cf060796c3e276cf0
katejim <katejimme@gmail.com>
2013-11-24 20:45:53 +0400

    load text from file. text in variable content. don't show anywhere

---
+++ js/loadSubs.js
@@ -0,0 +1,18 @@
+/**
+ * Created by KateKate on 24.11.13.
+ */
+ function handle_files(files) {
+       var f = files[0];
+       if (f) {
+          var r = new FileReader();
+          r.onload = function(e) {
+              var contents = e.target.result;
+            alert(contents);
+            p    = document.write("p" + contents + "</p>");
+          }
+          r.readAsText(f);
+       } else {
+          //alert("Failed to load file");
+       }
+    chrome.extension.sendRequest(payload, function(response) {});
+}
